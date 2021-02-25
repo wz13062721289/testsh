7516ed863a165184c7cdbfdb49a884166c467ced
wangzan <wangzan@chuanghehui.cn>
2021-02-25 13:10:34 +0800

    1.dev_zan 1

--- app/src/main/java/com/chh/gittest/MainActivity.kt
+++ app/src/main/java/com/chh/gittest/MainActivity.kt
@@ -3,6 +3,7 @@ package com.chh.gittest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 //我是dev
+//dev_zan 1
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
