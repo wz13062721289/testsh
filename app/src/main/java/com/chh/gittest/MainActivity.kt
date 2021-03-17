dee149983bf179636d279e81ce810b09b62054c9
wangzan <wangzan@chuanghehui.cn>
2021-03-17 11:06:17 +0800

    1.添加dev_sen  2次提交

--- app/src/main/java/com/chh/gittest/MainActivity.kt
+++ app/src/main/java/com/chh/gittest/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 //我是dev
 //我是dev_sen
+//我是dev_sen2
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
