e55543d32ea48631dfd17185ee4d2fa27a5907ef
wangzan <wangzan@chuanghehui.cn>
2021-03-18 17:13:54 +0800

    1.//dev_zan 7

--- app/src/main/java/com/chh/gittest/MainActivity.kt
+++ app/src/main/java/com/chh/gittest/MainActivity.kt
@@ -10,6 +10,7 @@ import android.os.Bundle
 //dev_zan 5
 //dev_zan 6
 class MainActivity : AppCompatActivity() {
+    //dev_zan 7
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
