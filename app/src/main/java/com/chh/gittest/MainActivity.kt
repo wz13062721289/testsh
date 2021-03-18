370290f7d890c9c2b1d4f24846cabf289f4bbc8a
wangzan <wangzan@chuanghehui.cn>
2021-03-18 14:42:16 +0800

    1. v1.0.0 tag

--- app/src/main/java/com/chh/gittest/MainActivity.kt
+++ app/src/main/java/com/chh/gittest/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 //我是main
 class MainActivity : AppCompatActivity() {
     // test tag
+    // v 1.0.0 tag
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
